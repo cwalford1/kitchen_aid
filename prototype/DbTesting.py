0a326a940f93f2db734633f948611610174fddd4
Carter Walford <cnwalford1@gmail.com>
2021-09-06 17:14:05 +0100

    tested out sqlite3

--- prototype/DbTesting.py
+++ prototype/DbTesting.py
@@ -1,2 +1,10 @@
 import sqlite3 as sl
 
+con = sl.connect("test.db")
+cur=con.cursor()
+
+#cur.execute("""CREATE TABLE testTable (f1 TEXT,f2 INTEGER ,f3 TEXT)""")
+#cur.execute("INSERT INTO testTable VALUES ('first',1,'second')")
+all=cur.execute("""select * FROM testTable""").fetchall()
+print(all)
+con.commit()

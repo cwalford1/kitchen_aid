9a34099ec0045da2bb3029fce778a97f7b5dac37
cwalford1 <cnwalford1@gmail.com>
2021-12-23 13:01:24 +0000

    testing hooks

--- app/main.py
+++ app/main.py
@@ -1,10 +1,44 @@
-from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.button import Button
+from kivy.metrics import dp
+from kivymd.uix.screen import MDScreen
+from kivy.uix.anchorlayout import AnchorLayout
 
-class MyApp(App):
+from kivymd.app import MDApp
+from kivymd.uix.button import MDRectangleFlatButton
+from kivymd.uix.datatables import MDDataTable
+
+COLUMN_DATA = [
+    ("Item Name", dp(30)),
+    ("In Stock?", dp(30)),
+    ("Recipes In", dp(30)), 
+    ("Importance", dp(20)),
+]
+ROW_DATA = [
+    (
+        "salt",
+        ("checkbox-marked-circle", [39 / 256, 174 / 256, 96 / 256, 1], "Yes"),
+        100,
+        4,
+    ),
+    ("mushrooms", ("alert-circle", [1, 0, 0, 1], "No"), 10, 3),
+    ("Coffee", ("alert", [255 / 256, 165 / 256, 0, 1], "Running Low"),
+    2,
+    5,
+    )
+]
+
+
+class MainApp(MDApp):
     def build(self):
-        return Button(text = 'test booton')
+        layout = AnchorLayout(anchor_x ="right",anchor_y = "bottom")
+        screen = MDScreen()
+        self.data_table = MDDataTable(
+            use_pagination=True, check=True, column_data=COLUMN_DATA, row_data=ROW_DATA,sorted_on="item name",sorted_order="ASC",elevation=2
+        )
+        layout.add_widget(self.data_table)
+        #screen.add_widget(self.data_table)
+        return layout
+    
 
-if __name__=='__main__':
-    MyApp().run()
+
+if __name__ == "__main__":
+    MainApp().run()
